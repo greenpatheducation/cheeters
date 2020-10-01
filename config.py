6a04b5c922e8e3aaa52e13afcd89ef6a93730636
shubham-natekar <shubham.natekar@greenpatheducation.com>
2020-10-01 23:32:49 +0530

    data updated

---
+++ config.py
@@ -0,0 +1,40 @@
+number = 15
+URL = 'https://github.com/greenpatheducation/cheeters/blob/cheetersBranch/data/'
+URL2= 'https://raw.githubusercontent.com/greenpatheducation/cheeters/cheetersBranch/data/'
+varg=['ucharan_1','ucharan_2','ucharan_3','ucharan_4','ucharan_5','ucharan_6','ucharan_7','ucharan_8','ucharan_9']
+
+akshars=[['swar_1','swar_2','swar_15','vayanjan_1','vayanjan_2','vayanjan_3','vayanjan_4','vayanjan_5','vayanjan_33'], 
+['swar_3','swar_4','vayanjan_6','vayanjan_7','vayanjan_8','vayanjan_9','vayanjan_10','vayanjan_26','vayanjan_30'],
+['swar_7','swar_8','vayanjan_11','vayanjan_12','vayanjan_13','vayanjan_14','vayanjan_15','vayanjan_27','vayanjan_31'],
+['swar_9','vayanjan_16','vayanjan_17','vayanjan_18','vayanjan_19','vayanjan_20','vayanjan_28','vayanjan_32'],
+['swar_5','swar_6','vayanjan_21','vayanjan_22','vayanjan_23','vayanjan_24','vayanjan_25'],
+['swar_14','vayanjan_10','vayanjan_15'],
+['swar_10','swar_11'],
+['swar_12','swar_13'],
+['vayanjan_29']]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+varg=['कंठ','तालु','मुद्रा','दंत','ओष्ठ','नासिका','कंठतलू','कन्ठोष्टय','दन्तोष्ठ्य']
+akshars=[['अ','आ','अः','क','ख','ग','घ','ङ','ह'],
+		  ['इ','ई','च','छ','ज','झ','ञ','य','श'],
+		  ['ट','ठ','ड','ढ','ण','र','ष'],
+		  ['त','थ','द','ध','न','ल','स'],
+		  ['प','फ','ब','भ','म'],
+		  ['अं','ण',],
+		  ['ए','ऐ'],
+		  ['ओ','औ'],
+		  ['व']]
+'''
